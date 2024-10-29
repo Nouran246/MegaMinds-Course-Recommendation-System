2dd411e8004c0b33e40e5838d70807a38431074e
Laila4563 <125660987+Laila4563@users.noreply.github.com>
2024-10-29 20:32:07 +0300

    pushhh

--- views/Users/meeting-details.php
+++ views/Users/meeting-details.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <?php
 session_start();
+include_once "../../public/includes/DB.php";
+
 ?>
   <head>
 
@@ -15,7 +17,6 @@ session_start();
 
     <!-- Bootstrap core CSS -->
     <link href="../../public/css/user css/bootstrap.min.css" rel="stylesheet">
-
 
     <!-- Additional CSS Files -->
     <link rel="stylesheet" href="../../public/css/user css/fontawesome.css">
@@ -62,7 +63,7 @@ https://templatemo.com/tm-569-edu-meeting
                           </li>
                           <li><a href="cart-page.php">Cart</a></li> 
                           <li><a href="Courses.php">My Profile</a></li> 
-                          <li><a href="index.php">Sign Out</a></li> 
+                          <li><a href="../../public/database/signout.php?action=signout">Sign Out</a></li> 
                       </ul>        
                       <a class='menu-trigger'>
                           <span>Menu</span>
@@ -100,17 +101,17 @@ https://templatemo.com/tm-569-edu-meeting
                   <div class="date">
                     <h6>Nov <span>12</span></h6>
                   </div>
-                  <a href="meeting-details.php"><img src="../../public/images/single-meeting.jpg" alt=""></a>
+                  <a href="meeting-details.php"><img src="../../public/images/AI.jpeg" alt=""></a>
                 </div>
                 <div class="down-content">
-                  <a href="meeting-details.php"><h4>Online Teaching and Learning Tools</h4></a>
-                  <p>Recreio dos Bandeirantes, Rio de Janeiro - RJ, 22795-008, Brazil</p>
+                  <a href="meeting-details.php"><h4>Introduction To AI</h4></a>
+                  <!-- <p>Recreio dos Bandeirantes, Rio de Janeiro - RJ, 22795-008, Brazil</p>
                   <p class="description">
                     This is an edu meeting HTML CSS template provided by <a href="https://templatemo.com/" target="_blank" rel="nofollow">TemplateMo website</a>. This is a Bootstrap v5.1.3 layout. If you need more free website templates like this one, please visit our website TemplateMo. Please tell your friends about our website. Thank you. If you want to get the latest collection of HTML CSS templates for your websites, you may visit <a rel="nofollow" href="https://www.toocss.com/" target="_blank">Too CSS website</a>. If you need a working contact form script, please visit <a href="https://templatemo.com/contact" target="_parent">our contact page</a> for more info.
                     
                     <br><br>You are allowed to use this edu meeting CSS template for your school or university or business. You can feel free to modify or edit this layout. You are not allowed to redistribute the template ZIP file on any other template website. Please contact us for more information.
-                  </p>
-                  <div class="row">
+                  </p> -->
+                  <!-- <div class="row">
                     <div class="col-lg-4">
                       <div class="hours">
                         <h5>Hours</h5>
@@ -138,7 +139,7 @@ https://templatemo.com/tm-569-edu-meeting
                           <li><a href="#">Twitter</a>,</li>
                           <li><a href="#">Linkedin</a>,</li>
                           <li><a href="#">Behance</a></li>
-                        </ul>
+                        </ul> -->
                         <div class="main-button-red">
                           <button onclick="window.location.href='cart-page.php'">Add To Cart</button>
                         </div>
@@ -156,11 +157,11 @@ https://templatemo.com/tm-569-edu-meeting
            
             <div class="container">
       
-              <div class="row text-center">		
+              <div class="row text-center">   
                 <h3 class="text-center">Learners Reviews</h3>
-                      <div class="col-sm-6 col-md-4">				
+                      <div class="col-sm-6 col-md-4">       
                                                 
-                        <div class="testimonial-box">			
+                        <div class="testimonial-box">     
                           <img src="https://images.pexels.com/photos/206615/pexels-photo-206615.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" class="img-responsive" alt="" width="90">
                           <div class="ratings-icons">
                             <span class="fa fa-star"></span>
@@ -170,13 +171,13 @@ https://templatemo.com/tm-569-edu-meeting
                             <span class="fa fa-star"></span>
                           </div>
                           <h4>James Baker</h4>
-                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sapien augue, dictum et gravida et</p>									 							
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sapien augue, dictum et gravida et</p>                                
                           
-                        </div>					
-                      </div> <!-- End Col -->								
+                        </div>          
+                      </div> <!-- End Col -->               
                       
-                      <div class="col-sm-6 col-md-4">																		
-                        <div class="testimonial-box">							
+                      <div class="col-sm-6 col-md-4">                                   
+                        <div class="testimonial-box">             
                           <img src="https://images.pexels.com/photos/478544/pexels-photo-478544.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" class="img-responsive" alt="" width="90">
                           <div class="ratings-icons">
                             <span class="fa fa-star"></span>
@@ -186,12 +187,12 @@ https://templatemo.com/tm-569-edu-meeting
                             <span class="fa fa-star"></span>
                           </div>
                           <h4>Jon Doe</h4>
-                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sapien augue, dictum et gravida et</p>									 							
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sapien augue, dictum et gravida et</p>                                
                           
-                        </div>					
-                      </div> <!-- End Col -->								
+                        </div>          
+                      </div> <!-- End Col -->               
                       
-                      <div class="col-sm-6 col-md-4">																		
+                      <div class="col-sm-6 col-md-4">                                   
                         <div class="testimonial-box">
                           <img src="https://images.pexels.com/photos/478544/pexels-photo-478544.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" class="img-responsive" alt="" width="90">
                           <div class="ratings-icons">
@@ -202,10 +203,10 @@ https://templatemo.com/tm-569-edu-meeting
                             <span class="glyphicon glyphicon-star"></span>
                           </div>
                           <h4>Maria Jose</h4>
-                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sapien augue, dictum et gravida et</p>									 							
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sapien augue, dictum et gravida et</p>                                
                           
-                        </div>					
-                      </div> <!-- End Col -->								
+                        </div>          
+                      </div> <!-- End Col -->               
                               
                     
                     </div>
@@ -223,7 +224,6 @@ https://templatemo.com/tm-569-edu-meeting
   </section>
   </section>
 
-
   <!-- Scripts -->
   <!-- Bootstrap core JavaScript -->
     <script src="../../public/js/user js/jquery/jquery.min.js"></script>
@@ -240,7 +240,8 @@ https://templatemo.com/tm-569-edu-meeting
 
 </body>
 
-
   </body>
 
 </html>
+
+
