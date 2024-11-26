55163c46343cc073eea82efd9f2603c61e77fc65
Nouran246 <111746732+Nouran246@users.noreply.github.com>
2024-11-26 14:03:47 +0200

    deployment

--- public/includes/DB.php
+++ public/includes/DB.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$DB = "megaminds";
+$servername = "sql211.infinityfree.com";
+$username = "if0_37790923";
+$password = " 8dTUdnid6i ";
+$DB = "if0_37790923_megaminds";
 
 $conn = mysqli_connect($servername,$username,$password,$DB);
 
