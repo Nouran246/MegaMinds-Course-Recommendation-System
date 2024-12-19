04efb84a190bf7df875186f86eb5ea669627b89c
Nouran246 <111746732+Nouran246@users.noreply.github.com>
2024-12-19 19:45:07 +0200

    fixing permissions
    Co-author: rowlkh

--- App/views/Admins/permissions.php
+++ App/views/Admins/permissions.php
@@ -54,13 +54,25 @@ $userTypes = fetchUserTypes($con);
     <title>Pages Assignment </title>
     <link
       rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-    />
-    <link rel="stylesheet" href="../../public/css/admin css/bootstrap.min.css" />
-    <link rel="stylesheet" href="../../public/css/admin css/site.css" />
-    <link rel="stylesheet" href="../../public/css/admin css/header.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.0/xlsx.full.min.js"></script>
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="../../../public/css/admin css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../../../public/css/admin css/site.css" />
+    <link rel="stylesheet" href="../../../public/css/admin css/header.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.0/xlsx.full.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+  
+    
+   
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+    />
+  
     <script>
         $(document).ready(function () {
             $("#btnLeft").click(function () {
